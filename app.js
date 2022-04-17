e37a76a38bf6736be3a28aa8a1617997b461fdeb
Yuri The Doer <eyeri91@gmail.com>
2022-04-17 19:30:54 +0200

    Make a new grid when the user enters new number

--- app.js
+++ app.js
@@ -1,36 +1,35 @@
 const container = document.querySelector('.container');
 const submit = document.querySelector('.btn.submit');
+const div = document.querySelector('div');
+const divs = document.querySelectorAll('.div');
 
 
-
+// Default divs. 16 by 16
 for (let i = 0; i < 256; i++) {
     let div = document.createElement('div')
     div.className = `div ${i}`;
     container.appendChild(div);
 }
 
-let divs = document.querySelectorAll('.div')
-
-divs.forEach(div => {
-    div.addEventListener('mouseover', changeColor);
-});
-
-function changeColor(e) {
-    e.target.classList.add("hovered");
-}
+// React to the user input submission
 
 submit.addEventListener('click', () => {
     let userInput = document.querySelector('#size');
     let newGridSize = userInput.value;
+    // grid size can't be more than 100 by 100
     deleteOldGrid();
     addNewDivs(newGridSize);
 });
+
+// Delete the old divs
 
 function deleteOldGrid() {
     while (container.firstChild) {
         container.removeChild(container.firstChild);
     }
 }
+
+// Add the new divs
 
 function addNewDivs(newGridSize) {
     let numOfDivs = newGridSize * newGridSize;
@@ -39,4 +38,21 @@ function addNewDivs(newGridSize) {
         div.className = `div ${i}`;
         container.appendChild(div);
     }
+    makeNewGrid(newGridSize);
+}
+
+// Make new right squared grid.
+function makeNewGrid(newGridSize) {
+    container.style.gridTemplateColumns = `repeat(${newGridSize}, 1fr)`;
+}
+
+
+// Change the color of hovered divs.
+
+divs.forEach(div => {
+    div.addEventListener('mouseover', changeColor);
+});
+
+function changeColor(e) {
+    e.target.classList.add("hovered");
 }
