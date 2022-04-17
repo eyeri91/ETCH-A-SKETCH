7b71bb20a35443602b9fe4197992d1ce3623c8b2
Yuri The Doer <eyeri91@gmail.com>
2022-04-17 19:13:39 +0200

    Delete old divs and add the new number of divs

--- app.js
+++ app.js
@@ -1,4 +1,7 @@
-const container = document.querySelector('.grid.container');
+const container = document.querySelector('.container');
+const submit = document.querySelector('.btn.submit');
+
+
 
 for (let i = 0; i < 256; i++) {
     let div = document.createElement('div')
@@ -6,8 +9,8 @@ for (let i = 0; i < 256; i++) {
     container.appendChild(div);
 }
 
-
 let divs = document.querySelectorAll('.div')
+
 divs.forEach(div => {
     div.addEventListener('mouseover', changeColor);
 });
@@ -16,6 +19,24 @@ function changeColor(e) {
     e.target.classList.add("hovered");
 }
 
-// Get all divs 
-// Write a function that whever the 'hover' happens
-// Log the very div className.
+submit.addEventListener('click', () => {
+    let userInput = document.querySelector('#size');
+    let newGridSize = userInput.value;
+    deleteOldGrid();
+    addNewDivs(newGridSize);
+});
+
+function deleteOldGrid() {
+    while (container.firstChild) {
+        container.removeChild(container.firstChild);
+    }
+}
+
+function addNewDivs(newGridSize) {
+    let numOfDivs = newGridSize * newGridSize;
+    for (let i = 0; i < numOfDivs; i++) {
+        let div = document.createElement('div')
+        div.className = `div ${i}`;
+        container.appendChild(div);
+    }
+}
