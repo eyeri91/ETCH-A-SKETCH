8699cf29203a5c3b5beac2324e09f151c4512fcd
Yuri The Doer <eyeri91@gmail.com>
2022-04-17 20:52:08 +0200

    Remove hovered class of the divs when any button is clicked

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const container = document.querySelector('.container');
+const buttons = document.querySelectorAll('.btn');
 const submit = document.querySelector('.btn.submit');
 const monoBtn = document.querySelector('.mono');
 const pastelBtn = document.querySelector('.pastel');
@@ -17,7 +18,7 @@ for (let i = 0; i < 256; i++) {
 // React to the user input submission
 
 submit.addEventListener('click', () => {
-    let userInput = document.querySelector('#size');
+    let userInput = document.querySelector('#size').value;
     let newGridSize = (userInput.value <= 100 ? userInput.value : 100);
     deleteOldGrid();
     addNewDivs(newGridSize);
@@ -46,10 +47,6 @@ function addNewDivs(newGridSize) {
 // Make new right squared grid.
 function makeNewGrid(newGridSize) {
     container.style.gridTemplateColumns = `repeat(${newGridSize}, 1fr)`;
-    let divs = document.querySelectorAll('.div');
-    divs.forEach(div => {
-        div.addEventListener('mouseover', changeColor);
-    });
 }
 
 
@@ -62,3 +59,33 @@ divs.forEach(div => {
 function changeColor(e) {
     e.target.classList.add("hovered");
 }
+
+// any color button is clicked
+//  Remove any class contains 'hovered'
+// Add new hoveredcolor;
+
+
+buttons.forEach(btn => {
+    btn.addEventListener('click', removeHovered);
+})
+
+function removeHovered() {
+    let divs = document.querySelectorAll('.div');
+    divs.forEach(div => {
+        if (div.classList.contains('hovered')) {
+            div.classList.remove('hovered');
+        }
+    })
+}
+
+
+
+// monoBtn.addEventListener('click', changeToMono);
+
+// function changeToMono() {
+
+// }
+
+// pastelBtn.addEventListener('click', changeToPastel);
+// randomBtn.addEventListener('click', changeToRandom);
+// resetBtn.addEventListener('click', backToDefault);
